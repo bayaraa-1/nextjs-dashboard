5363bf9ebc556dc5bd2442a44a2c94e89d6f3f76
bayaraa-1 <mongolasia724@gmail.com>
2023-10-31 21:32:06 +0800

    edit .env auth.config

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,5 @@
 import type { NextAuthConfig } from "next-auth";
+import { NextResponse } from "next/server";
 
 export const authConfig = {
   providers: [],
@@ -8,12 +9,14 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
+      console.log("isLoggedIn === ", isLoggedIn, !!auth?.user);
+      console.log("isLoggedIn auth.user === ", !!auth?.user);
       const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL("/dashboard", nextUrl));
+        return NextResponse.redirect(new URL("/dashboard", nextUrl));
       }
       return true;
     },
